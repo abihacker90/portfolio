7b9e031d0680279a98bf9791d450f58533596454
Adam Bihacker <adam.bihacker@gmail.com>
2022-07-28 15:06:32 +0200

    basics components added

---
+++ portfolio/src/components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <>
+            <div className='container'>
+                <h1>Adam Bihacker</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About me</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+
+export default Nav;
